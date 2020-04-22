3d4c69f30332339bd6d950ee1039a8f6fd974521
Вещев Владислав <bloodlad.hacker@yandex.ru>
2020-04-22 16:15:14 +0300

    testing feedback

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mailer = require('./nodemailer');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = 80;
 let feedbackInfo = undefined;       // Для временного хранения данных формы
 
 // строка как я понял подключает сам bodyparser
